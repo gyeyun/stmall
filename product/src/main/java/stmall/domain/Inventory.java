23c2b8385611ba5d856e15862cf3cf21f06d73e3
gyeyun <147613387+gyeyun@users.noreply.github.com>
2025-06-10 13:34:26 +0900

    commit

---
+++ product/src/main/java/stmall/domain/Inventory.java
@@ -0,0 +1,36 @@
+package stmall.domain;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import javax.persistence.*;
+import lombok.Data;
+import stmall.ProductApplication;
+import stmall.domain.StockDecreased;
+import stmall.domain.StockIncreased;
+
+@Entity
+@Table(name = "Inventory_table")
+@Data
+//<<< DDD / Aggregate Root
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private Integer stock;
+
+    public static InventoryRepository repository() {
+        InventoryRepository inventoryRepository = ProductApplication.applicationContext.getBean(
+            InventoryRepository.class
+        );
+        return inventoryRepository;
+    }
+}
+//>>> DDD / Aggregate Root
